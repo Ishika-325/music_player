a1c648a77288803b64473e0a4e10583eee521fc4
Ishika-325 <gishika325@gmail.com>
2025-10-26 06:57:39 +0530

    error handling nav-menu

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -4,11 +4,13 @@ import { motion } from "motion/react";
 
 
 
-const transition = {
+import { Transition } from "motion/react";
+
+const transition: Transition = {
   type: "spring",
   mass: 0.5,
-  damping: 11.5,
-  stiffness: 100,
+  damping: 12,
+  stiffness: 120,
   restDelta: 0.001,
   restSpeed: 0.001,
 };
